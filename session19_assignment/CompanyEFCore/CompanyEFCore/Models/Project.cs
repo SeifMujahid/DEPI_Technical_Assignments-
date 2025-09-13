3ec60ac21a520470f29ed87c12d236281ce408a4
SeifMujahid <112726583+SeifMujahid@users.noreply.github.com>
2025-09-13 14:19:16 +0300

    CompanyEFCore_EF_Task1

---
+++ session19_assignment/CompanyEFCore/CompanyEFCore/Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEFCore.Models;
+
+[Table("Project")]
+public partial class Project
+{
+    [StringLength(50)]
+    public string? Pname { get; set; }
+
+    [Key]
+    public int Pnumber { get; set; }
+
+    [StringLength(50)]
+    public string? Plocation { get; set; }
+
+    [StringLength(50)]
+    public string? City { get; set; }
+
+    public int? Dnum { get; set; }
+
+    [ForeignKey("Dnum")]
+    [InverseProperty("Projects")]
+    public virtual Department? DnumNavigation { get; set; }
+
+    [InverseProperty("PnoNavigation")]
+    public virtual ICollection<WorksFor> WorksFors { get; set; } = new List<WorksFor>();
+}
