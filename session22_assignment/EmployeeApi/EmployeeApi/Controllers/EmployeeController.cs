405557843f3528b599a93b82b1aab241ae8cb19c
SeifMujahid <112726583+SeifMujahid@users.noreply.github.com>
2025-09-15 21:22:32 +0300

    EmployeeApi_Api_Task1

---
+++ session22_assignment/EmployeeApi/EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using EmployeeApi.Data;
+using EmployeeApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private ApplicationDbContext _DbContext;
+        public EmployeeController(ApplicationDbContext _DbContext)
+        {
+            this._DbContext = _DbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employees = _DbContext.Employees.ToList();
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            _DbContext.Employees.Add(employee);
+            _DbContext.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id, Employee employee)
+        {
+            var existingEmployee = _DbContext.Employees.Find(id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+            existingEmployee.Name = employee.Name;
+            existingEmployee.Email = employee.Email;
+            existingEmployee.Phone = employee.Phone;
+            existingEmployee.Salary = employee.Salary;
+            _DbContext.SaveChanges();
+            return Ok(existingEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var existingEmployee = _DbContext.Employees.Find(id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+            _DbContext.Employees.Remove(existingEmployee);
+            _DbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ session22_assignment/EmployeeApi/EmployeeApi/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
