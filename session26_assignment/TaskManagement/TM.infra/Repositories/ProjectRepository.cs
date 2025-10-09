d0850b94d9d3c0fa40d5fe99f6800a089036f0f3
SeifMujahid <112726583+SeifMujahid@users.noreply.github.com>
2025-10-09 03:03:50 +0300

    TaskManagement_Api_Task4

---
+++ session26_assignment/TaskManagement/TM.infra/Repositories/ProjectRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TM.app.Interfices;
+using TM.core.Entities;
+using TM.infra.Data;
+
+namespace TM.infra.Repositories
+{
+    public class ProjectRepository:IProjectRepository
+    {
+        private readonly TMDbContext _context;
+        public ProjectRepository(TMDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Project> RepoAddProject(Project newProject)
+        {
+            if(newProject != null)
+            {
+                await _context.Projects.AddAsync(newProject);
+                await _context.SaveChangesAsync();
+            }
+            return newProject;
+        }
+
+        public async Task<Project> RepoDeleteProjectById(int Id)
+        {
+            Project project = await _context.Projects.FindAsync(Id);
+            if(project != null)
+            {
+                _context.Projects.Remove(project);
+                await _context.SaveChangesAsync();
+            }
+            return project;
+        }
+
+        public async Task<IEnumerable<Project>> RepoGetAllProjects()
+        {
+            return await Task.FromResult(_context.Projects.ToList());
+        }
+
+        public async Task<Project?> RepoGetProjectById(int Id)
+        {
+            Project? project = await _context.Projects.FindAsync(Id);
+            return project;
+        }
+
+        public async Task<Project> RepoUpdateProject(Project updatedProject)
+        {
+            Project target = await _context.Projects.FindAsync(updatedProject.Id);
+            if (target != null && updatedProject != null)
+            {
+                target.Title=updatedProject.Title;
+                target.Description=updatedProject.Description;
+                target.Tasks=updatedProject.Tasks;
+               await _context.SaveChangesAsync();
+            }
+            return target;
+        }
+    }
+}
